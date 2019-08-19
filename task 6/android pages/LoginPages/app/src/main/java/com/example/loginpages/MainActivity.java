4b97938da189f88611a1e4ec30ee1e78b4ec9e0f
namrata <nambrathaspatil@gmail.com>
2019-08-19 21:44:16 +0530

    Android login page

---
+++ task 6/android pages/LoginPages/app/src/main/java/com/example/loginpages/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.loginpages;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    Button login;
+    ImageView i1;
+    ImageView i2;
+    TextView i3;
+    TextView i4;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        login = findViewById(R.id.button1);
+        i1=findViewById(R.id.imageView);
+        i2=findViewById(R.id.imageView2);
+        i3=findViewById(R.id.textView2);
+        i4=findViewById(R.id.textView3);
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                i1.setVisibility(1);
+                i2.setVisibility(1);
+                i3.setVisibility(1);
+                i4.setVisibility(1);
+            }
+        });
+    }
+}
